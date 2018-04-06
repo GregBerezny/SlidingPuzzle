9c008fc6e008d6aa59465486fc7b2ba0030413d8
Eeshan Garg <jerryguitarist@gmail.com>
2018-04-06 00:37:55 -0230

    Refactor getAvailableActions.

--- AI.js
+++ AI.js
@@ -2,33 +2,31 @@ AI = function (grid) {
     var self = {};
 
     self.grid = grid;
+    self.actions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
     self.getNextAction = function() {
-        let actions = self.getAvailableActions();
+        let actions = self.getLegalActions();
         let rand = Math.floor(Math.random() * actions.length);
         return actions[rand];
     }
 
-    self.getAvailableActions = function() {
+    self.getLegalActions = function() {
         let emptySpace = self.grid.getPosition(0);
         let x = emptySpace[0];
         let y = emptySpace[1];
 
-        let actions = [];
-        if (!gui.isOOB(x+1, y)) {
-            actions.push([x+1, y]);
-        }
-        if (!gui.isOOB(x-1, y)) {
-            actions.push([x-1, y]);
-        }
-        if (!gui.isOOB(x, y+1)) {
-            actions.push([x, y+1]);
-        }
-        if (!gui.isOOB(x, y-1)) {
-            actions.push([x, y-1]);
+        let availableActions = [];
+
+        for (var i = 0; i < self.actions.length; i++) {
+            let nx = x + self.actions[i][0];
+            let ny = y + self.actions[i][1];
+
+            if (!self.grid.isOOB(nx, ny)) {
+                availableActions.push([nx, ny]);
+            }
         }
 
-        return actions;
+        return availableActions;
     }
 
     return self;
