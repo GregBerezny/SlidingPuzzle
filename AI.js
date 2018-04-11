6090ce8e0142d3acdaef43b8c548eae16c05d8b2
Eeshan Garg <jerryguitarist@gmail.com>
2018-04-11 00:41:55 -0230

    Add Node class; pass state to getLegalActions.

--- AI.js
+++ AI.js
@@ -5,13 +5,13 @@ AI = function (grid) {
     self.actions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
     self.getNextAction = function() {
-        let actions = self.getLegalActions();
+        let actions = self.getLegalActions(self.grid);
         let rand = Math.floor(Math.random() * actions.length);
         return actions[rand];
     }
 
-    self.getLegalActions = function() {
-        let emptySpace = self.grid.getPosition(0);
+    self.getLegalActions = function(state) {
+        let emptySpace = state.getPosition(0);
         let x = emptySpace[0];
         let y = emptySpace[1];
 
@@ -29,5 +29,15 @@ AI = function (grid) {
         return availableActions;
     }
 
+    return self;
+}
+
+Node = function (board, parent, action, g, h) {
+    var self = {};
+    self.state = state;
+    self.action = action;
+    self.parent = parent;
+    self.g = g;
+    self.h = h;
     return self;
 }
